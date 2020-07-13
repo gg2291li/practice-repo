51fb5e0d62adeef2f185c044558ab8256ddbad8e
unknown <gg2291li@gmail.com>
2020-07-12 22:44:40 -0400

    my solution for question 26, Remove Duplicates

---
+++ Leetcode_26(Remove Duplicate).py
@@ -0,0 +1,14 @@
+# My solution for Leetcode question 26, Remove Duplicates from Sorted Array
+
+class Solution:
+    def removeDuplicates(self, nums):
+        if len(nums) == 0: return nums
+        current = "placeholder"
+        position = 0
+
+        for index in range(len(nums)):
+            if nums[index] != current:
+                nums[position] = nums[index]
+                current = nums[index]
+                position += 1
+        return nums
