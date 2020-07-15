688a2ef54323c2ecef46016e897e9693c0b4947c
unknown <gg2291li@gmail.com>
2020-07-14 23:58:39 -0400

    my solution for question 34, Find first and last in sorted list

--- Leetcode_34(First and Last Position of ELement in Sorted Array).py
+++ Leetcode_34(First and Last Position of ELement in Sorted Array).py
@@ -1,16 +1,26 @@
 class Solution:
-    def searchRange(self, nums, target):
-        if len(nums) == 0: return [-1, -1]
-        lIndent = 0
-        rIndent = len(nums) - 1
+    # returns leftmost (or rightmost) index at which `target` should be inserted in sorted
+    # array `nums` via binary search.
+    def extreme_insertion_index(self, nums, target, left):
+        lo = 0
+        hi = len(nums)
 
-        while True:
-            if rIndent - lIndent <= 1:
-                if nums[lIndent] == target and nums[rIndent] == target: return [lIndent, rIndent]
-                if nums[lIndent] == target: return [lIndent, lIndent]
-                if nums[rIndent] == target: return [rIndent, rIndent]
-                return [-1, -1]
-            
-            if (nums[lIndent] == target and nums[rIndent] == target) and (lIndent == 0 or nums[lIndent - 1] < target) and (rIndent == len(rIndent) - 1 and nums[rIndent + 1] > target):
-                return [lIndent, rIndent]
-            
+        while lo < hi:
+            mid = (lo + hi) // 2
+            if nums[mid] > target or (left and target == nums[mid]):
+                hi = mid
+            else:
+                lo = mid+1
+
+        return lo
+
+
+    def searchRange(self, nums, target):
+        left_idx = self.extreme_insertion_index(nums, target, True)
+
+        # assert that `left_idx` is within the array bounds and that `target`
+        # is actually in `nums`.
+        if left_idx == len(nums) or nums[left_idx] != target:
+            return [-1, -1]
+
+        return [left_idx, self.extreme_insertion_index(nums, target, False)-1]
