32c18ca785a2f942a9eae8c8ff70dfff5a4d2688
unknown <gg2291li@gmail.com>
2020-07-15 17:43:00 -0400

    test

---
+++ Leetcode_35(Seach Insert Position).py
@@ -0,0 +1,9 @@
+class Solution:
+    def searchInsert(self, nums, target):
+        if len(nums) == 0: return 0
+        lBound = 0
+        rBound = len(nums) - 1
+
+        while True:
+            mid = (rBound - lBound) // 2
+            if target == lBound + mid: return lBound + mid
