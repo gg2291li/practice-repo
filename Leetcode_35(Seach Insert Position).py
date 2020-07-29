e95af6bb8c31b611109a9db6c190bd3501604060
unknown <gg2291li@gmail.com>
2020-07-29 01:07:17 -0400

    my solution for question 35

--- Leetcode_35(Seach Insert Position).py
+++ Leetcode_35(Seach Insert Position).py
@@ -6,5 +6,15 @@ class Solution:
 
         while True:
             mid = (rBound - lBound) // 2
-            if target == lBound + mid: return lBound + mid
+            if target == nums[lBound + mid]: return lBound + mid
+            elif target < nums[lBound + mid]:
+                if lBound + mid == 0 or target > nums[lBound + mid -1]:
+                    return lBound + mid
+                else:
+                    rBound = lBound + mid - 1
+            else:
+                if lBound + mid == len(nums) - 1 or target < nums[lBound + mid + 1]:
+                    return lBound + mid + 1
+                else:
+                    lBound = lBound + mid + 1
             # test 5
